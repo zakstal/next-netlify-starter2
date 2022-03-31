84320c1663076e0126c1c5c64a003ac982be405e
Zak Stallings <zakstal@gmail.com>
2022-03-31 11:37:39 -0600

    finish this

--- pages/index.js
+++ pages/index.js
@@ -11,9 +11,9 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Can we?" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          We can do this!
         </p>
       </main>
 
